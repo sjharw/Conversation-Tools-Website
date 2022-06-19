e11bd7d54da3c074801a0fd988f1aefbeb13a4a2
Sarah Harwood <sarahjharwood@googlemail.com>
2022-06-19 17:39:50 +0100

    js folder added

---
+++ js/base.js
@@ -0,0 +1,8 @@
+  // SIDENAV SLIDE OUT 
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "150px"; // Width of sidenav
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
