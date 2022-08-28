8e37e8593bf1adaed78f2cd8aff5555e13ac2fa0
Sarah Jane Harwood <sarahjharwood@googlemail.com>
2022-08-28 07:50:56 +0100

    styling & formatting

--- view.py
+++ view.py
@@ -19,14 +19,13 @@ app = Flask(__name__)
 
 
 @app.route("/")
-
-
 @app.route("/se")
 def se():
     """
     se
     """
     return render_template("/safeenvironment.html")
+
 
 @app.route("/shn")
 def shn():
@@ -35,12 +34,14 @@ def shn():
     """
     return render_template("/sixhumanneeds.html")
 
+
 @app.route("/sp")
 def sp():
     """
     sp
     """
     return render_template("/sharedpool.html")
+
 
 @app.route("/ha")
 def ha():
@@ -50,17 +51,7 @@ def ha():
     return render_template("/hiddenagendas.html")
 
 
-
 # run app
 if __name__ == "__main__":
-    port = int (
-        os.environ.get(
-            "PORT",
-            5000
-        )
-    )
-    app.run(
-        debug= True,
-        host= "0.0.0.0",
-        port= port
-    )
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
